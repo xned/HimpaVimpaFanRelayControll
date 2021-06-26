eda9a1f63bea191eaf8b5c7620f081f13973ac54
Christoffer <root@system-dojo.org>
2021-06-26 15:17:20 +0200

    initial comit, copy from older relay code

---
+++ HimpaVimpaFanRelayControll.ino
@@ -0,0 +1,77 @@
+/*
+ * Documentation: https://www.mysensors.org
+ * Support Forum: https://forum.mysensors.org
+ *
+ * https://www.mysensors.org/build/relay
+ */
+
+#define MY_DEBUG
+#define MY_RADIO_NRF24
+#define MY_REPEATER_FEATURE
+#define MY_NODE_ID 1
+#include <SPI.h>
+#include <MySensors.h>
+#include <Bounce2.h>
+
+#define RELAY_PIN  4
+#define BUTTON_PIN  3
+#define CHILD_ID 1
+#define RELAY_ON 1
+#define RELAY_OFF 0
+
+Bounce debouncer = Bounce();
+bool state = false;
+bool initialValueSent = false;
+
+MyMessage msg(CHILD_ID, V_STATUS);
+
+void setup()
+{
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  debouncer.attach(BUTTON_PIN);
+  debouncer.interval(10);
+
+  // Make sure relays are off when starting up
+  digitalWrite(RELAY_PIN, RELAY_OFF);
+  pinMode(RELAY_PIN, OUTPUT);
+}
+
+void presentation()  {
+  sendSketchInfo("Relay+button", "1.0");
+  present(CHILD_ID, S_BINARY);
+}
+
+void loop()
+{
+  if (!initialValueSent) {
+    Serial.println("Sending initial value");
+    send(msg.set(state?RELAY_ON:RELAY_OFF));
+    Serial.println("Requesting initial value from controller");
+    request(CHILD_ID, V_STATUS);
+    wait(2000, C_SET, V_STATUS);
+  }
+  if (debouncer.update()) {
+    if (debouncer.read()==LOW) {
+      state = !state;
+      // Send new state and request ack back
+      send(msg.set(state?RELAY_ON:RELAY_OFF), true);
+    }
+  }
+}
+
+void receive(const MyMessage &message) {
+  if (message.isAck()) {
+     Serial.println("This is an ack from gateway");
+  }
+
+  if (message.type == V_STATUS) {
+    if (!initialValueSent) {
+      Serial.println("Receiving initial value from controller");
+      initialValueSent = true;
+    }
+    // Change relay state
+    state = (bool)message.getInt();
+    digitalWrite(RELAY_PIN, state?RELAY_ON:RELAY_OFF);
+    send(msg.set(state?RELAY_ON:RELAY_OFF));
+  }
+}
